7203162280933107dee3fe707a054b5cf0f41635
Vaishnavmanoj <84540554+Vaishnavmanoj@users.noreply.github.com>
2021-09-03 22:28:25 +0300

    Update project vsc.py

--- project vsc.py
+++ project vsc.py
@@ -1,182 +1,219 @@
-from tkinter import *
-from tkinter import ttk
+import mysql.connector
 import random
 import time
-import datetime
-from tkinter import messagebox
-import mysql.connector
+import string  
 
+med_list = ['X12', 'X32', 'Y12', 'X44']
+mydb = mysql.connector.connect(user='root', password='root', database='hospital', autocommit=True)
 
-class Hospital:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Hospital Management System")
-        self.root.geometry("1540x800+0+0")
+cur = mydb.cursor()
 
-        lbltitle = Label(self.root, bd=20, relief=RIDGE, text="Hospital Management System",
-                         fg="red", bg="white", font=("times new roman", 50, "bold"))
-        lbltitle.pack(side=TOP, fill=X)
+print("Welcome to Hamad Hospital!\n")
+run = True
+globalName = 0
 
-        # =============Dataframe========
-        Dataframe = Frame(self.root, bd=20, relief=RIDGE)
-        Dataframe.place(x=0, y=130, width=1530, height=400)
+def random_string(letter_count, digit_count):  
+    str1 = ''.join((random.choice(string.ascii_letters) for x in range(letter_count)))  
+    str1 += ''.join((random.choice(string.digits) for x in range(digit_count)))  
+  
+    sam_list = list(str1) # it converts the string to list.  
+    random.shuffle(sam_list) # It uses a random.shuffle() function to shuffle the string.  
+    final_string = ''.join(sam_list)  
+    return final_string  
+def getDoctorOrPatientFromInput(activity):
+        docOrPatient = int(input(f"[1: Doctor\t2: Patient]\nWould you like to {activity} as a doctor or a patient? "))
+        Occupation = 0
+        if docOrPatient == 1:
+            Occupation = "doctor"
+        elif docOrPatient == 2:
+            Occupation = "patient"
+        return Occupation
+            
+def checkIfRegisteredUserExists(docOrPatient, name):
+    checkingIfNameAlreadyExists = (f"select {docOrPatient} from hos{docOrPatient} where {docOrPatient}= '{name}'")
+    cur.execute(checkingIfNameAlreadyExists)
+    getUser = cur.fetchone()
+    userExists = False
+    if getUser is not None:
+        userExists = True
+        return userExists
 
-        DataFrameLeft = LabelFrame(Dataframe, bd=10, relief=RIDGE, padx=10,
-                                   font=("times new roman", 12, "bold"), text="Patient Information")
-        DataFrameLeft.place(x=0, y=5, width=980, height=350)
+def login_to_an_existing_account():
+        occupation = getDoctorOrPatientFromInput("login")
+        if occupation == "doctor":
+            username = input("Please enter your name to validate your details: ")
+            globalName = username
+            doesExist = checkIfRegisteredUserExists(occupation, username)
+            if doesExist == None:
+                print("Your login details are nowhere to be found...")
+                register = input("Would you like to register? ")
+                if register == "y": registration(occupation)
+            else:
+                commonMenu(occupation)
+        elif occupation == "patient":
+            username = input("Please enter your name to validate your details: ")
+            globalName = username
+            doesExist = checkIfRegisteredUserExists(occupation, username)
+            if doesExist == None:
+                print("Your login details are nowhere to be found...")
+                register = input("Would you like to register? ")
+                if register == "y": registration(occupation)
+            else: 
+                commonMenu(occupation)
 
-        DataFrameRight = LabelFrame(Dataframe, bd=10, relief=RIDGE, padx=10,
-                                    font=("times new roman", 12, "bold"), text="Prescription")
-        DataFrameRight.place(x=990, y=5, width=460, height=350)
+def registration(docOrPatient):
+    print("Beginning registration...")
+    time.sleep(random.randrange(0, 3))
+    name = input("Enter your name: ")
+    globalName = name
+    doesExist = checkIfRegisteredUserExists(docOrPatient, name)
+    if doesExist == True:
+        print("Your data already exists in the database.")
+    else:
+        print("Your account has been created successfully... Please provide the following details to complete the registration!")
+        age = int(input("Enter your age: "))
+        phone = int(input("Enter your phone number: (+974) ---------"))
+        med = random.choice(med_list)
+        if docOrPatient == "doctor":
+            print({name, age, phone, med})
+            print("Generating your ID")
+            time.sleep(1)
+            ID = random_string(8, 4)
+            print(f"Your randomly generated ID is: [{ID}]")
+            command = (f"insert into hosdoctor (doctor, age, id, phone) values({name}, {age}, {ID}, {phone})")
+            cur.execute(command)
+        if docOrPatient == "patient":
+            dis = input("Enter your disease: ")
+            command = (f"insert into hospatient values({name}, {age}, {dis}, {phone}, {med})")
+            cur.execute(command)
+        print("Successful Registration...\nYou'll be logged in shortly!")
+        login_to_an_existing_account()
 
-        # ================ buttons frame============
-        Buttonframe = Frame(self.root, bd=20, relief=RIDGE)
-        Buttonframe.place(x=0, y=530, width=1530, height=70)
-
-        # ================ Details frame============
-        Detailsframe = Frame(self.root, bd=20, relief=RIDGE)
-        Detailsframe.place(x=0, y=600, width=1530, height=190)
-
-        # =============================== DataFrameLeft
-
-        lblNameTablet = Label(DataFrameLeft, text="Names of Tablet", font=(
-            "times new roman", 12, "bold"), padx=2, pady=6)
-        lblNameTablet.grid(row=0, column=0)
-
-        comNameTablet = ttk.Combobox(DataFrameLeft, font=(
-            "times new roman", 12, "bold"), width=33)
-        comNameTablet["values"] = (
-            "Nice", "Corona Vaccine", "Acetaminophen", "Adderall", "Amlodipine", "Ativan")
-        comNameTablet.grid(row=0, column=1)
-
-        lblref = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Refence No:", padx=2)
-        lblref.grid(row=1, column=0, sticky=W)
-        txtref = Entry(DataFrameLeft, font=("arial", 13, "bold"), width=35)
-        txtref.grid(row=1, column=1)
-        lbDose = Label(DataFrameLeft, font=("arial", 12, "bold"),
-                       text="Dose: ", padx=2, pady=4)
-        lbDose.grid(row=2, column=0, sticky=W)
-        txtDose = Entry(DataFrameLeft, font=("arial", 13, "bold"), width=35)
-        txtDose.grid(row=2, column=1)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="No of Tablets: : ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=3, column=0, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=3, column=1)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Lot: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=4, column=0, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=4, column=1)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Issue Date: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=5, column=0, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=5, column=1)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Expiry Date: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=6, column=0, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=6, column=1)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Daily Dose: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=7, column=0, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=7, column=1)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Side Effect: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=8, column=0, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=8, column=1)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Further Information: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=0, column=2, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=0, column=3)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Blood Pressure: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=1, column=2, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=1, column=3)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Storage Advice: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=2, column=2, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=2, column=3)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Medication: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=3, column=2, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=3, column=3)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Patient ID: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=4, column=2, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=4, column=3)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="NHS Number: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=5, column=2, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=5, column=3)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Patient Name: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=6, column=2, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=6, column=3)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Date of Birth: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=7, column=2, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=7, column=3)
-
-        lblNoOfTablets = Label(DataFrameLeft, font=(
-            "arial", 12, "bold"), text="Patient Address: ", padx=2, pady=6)
-        lblNoOfTablets.grid(row=8, column=2, sticky=W)
-        txtNoOftablets = Entry(DataFrameLeft, font=(
-            "arial", 13, "bold"), width=35)
-        txtNoOftablets.grid(row=8, column=3)
-
-        # ===============Dataframe Right====================
-        self.txtPrescription = Text(DataFrameRight, font=(
-            "arial", 12, "bold"), width=45, height=16, padx=2, pady=6)
-        self.txtPrescription.grid(row=0, column=0)
-
-        # ======================Buttons================
-        btnPrescription = Button(Buttonframe, text="Prescription", bg="green", fg="white", font=(
-            "arial", 12, "bold"), width=23, height=16, padx=2, pady=6)
-        btnPrescription.grid(row=0, column=0)
-
+def commonMenu(occupation):
+    mainMenu = {}
+    def task(task_fn):
+        mainMenu[task_fn.__name__] = task_fn
+    def delete_account(occupation):
+        print("Deleting your account")
+        cur.execute(f"DELETE FROM hos{occupation} WHERE {occupation}_name = '" + str(globalName) + "'")
+        print("Account has been deleted successfully!")
         
 
-root = Tk()
-ob = Hospital(root)
-root.mainloop()
+    def view_personal_details(occupation):
+        print("Name  Age  Disease  Phone No.  Meds ")        
+        command3 = (f"select*from hos{occupation} where {occupation}_name='" + str(globalName) + "'")
+        cur.execute(command3)
+        result = cur.fetchall()
+        for i in result:
+            print(f"i[0] i[1] i[2] i[3] i[4]")
+
+    def write_feedback():
+        patientName = input("Enter name of the patient: ")
+        doesExist = checkIfRegisteredUserExists("patient", patientName)
+        if doesExist == None:
+            print("Patient records not found!")
+        else:
+            print(f"Creating a file for {patientName} if it doesn't exist!")
+            file = open(f"{patientName} - Doctor's Advice.txt", "w")
+            review = input("Enter your review [add a fullstop to split the lines!]: ")
+            splitReviews = review.split(".")
+            file.writelines(splitReviews)
+            print("Review written successfully!")
+            print("\n".join(splitReviews))
+
+    def request_consultation_fee():
+        patientName = input("Enter name of the patient: ")
+        if doesExist == None:
+            print("Patient records not found!")
+        else:
+            fee = int(input("Enter the fee that you require: "))
+            feeQuery = (f"UPDATE hospatient (patientName) SET fee = {fee} WHERE patient == {patientName}")
+            print(f"Fee required updated!\n {patientName} is now required to pay {fee}")
+
+    def view_all_registered_patients():
+        print("Fetching data...")
+        cur.execute(f"select * from hospatient")
+        records = cur.fetchall()
+        time.sleep(random.randrange(0, 3))
+        print(f"Records of [{len(records)}] patients found!")
+        patientID = 1
+        for x in records:
+            print(f"---------------- [{patientID}] - Patient Name: {x[0]}\nPatient Age: {x[1]}\nPatient Disease: {x[2]}\nPatient Phone number: {x[3]}\nMedicines used: {x[4]}")
+            patientID+=1
+            time.sleep(random.randrange(0, 3))
+
+    def view_all_registered_doctors():
+        print("Fetching data...")
+        cur.execute(f"select * from hosdoctor")
+        records = cur.fetchall()
+        time.sleep(random.randrange(0, 3))
+        print(f"Records of [{len(records)}] doctors found!")
+        doctorID = 1
+        for x in records:
+            print(f"---------------- [{doctorID}] - Doctor Name: {x[0]}\nDoctor Age: {x[1]}\nDoctor ID: {x[2]}\nDoctor Phone number: {x[3]}")
+            Doctor+=1
+            time.sleep(random.randrange(0, 3))
+    def make_payment():
+        name = input("Enter your name: ")
+        if name != globalName:
+            print("Incorrect details")
+          
+        print("Enter payment method ""\n""1:Card only""\n")
+        pay = int(input("Enter your choice "))
+        if pay == 1:
+            card_det = input("Enter card details ")
+            card_pwd = int(input("Enter card pin "))
+            card_amount = int(input("Enter amount pending($) "))
+            print("Sending...")
+            time.sleep(random.randrange(0, 3))
+            print("Amount Sent! \/")
+    
+    def read_feedback():
+        print("Here is our Doctor's view on your health")
+        with open(f"{globalName}", 'r') as file:
+            data = file.readlines()
+            for j in data:
+                print(j, end="")
+    
+    mainMenu = {'1': delete_account, '2': view_personal_details}
+    doctorMenu = {'3': write_feedback, '4': request_consultation_fee, '5': view_all_registered_patients}   
+    patientMenu = {'3': make_payment, '4': read_feedback}   
+    completeMenu = mainMenu.copy()
+    if occupation == "doctor":
+        completeMenu.update(doctorMenu)
+    if occupation == "patient":
+        completeMenu.update(patientMenu)
+    for key, value in completeMenu.items():
+        print(f"{key} : {value.__name__ }")
+    MenuInput = int(input("Make your choice: "))
+    try:
+        completeMenu[f"{MenuInput}"](occupation)
+    except TypeError:
+        completeMenu[f"{MenuInput}"]()
+
+def register_new_account():
+    try:
+        registration(getDoctorOrPatientFromInput("register"))
+    except Exception as error:
+        print("Error occured! ")
+        print(error)
+
+    
+while run:
+    cur = mydb.cursor()
+    cur.execute("DROP DATABASE hospital")
+
+    cur.execute("CREATE DATABASE hospital")
+    cur.execute("USE hospital")
+    
+    cur.execute("CREATE TABLE IF NOT EXISTS hospatient (patient VARCHAR(255), age VARCHAR(255), dis VARCHAR(255), phone VARCHAR(255), med VARCHAR(255), fee VARCHAR(100))")
+    cur.execute("CREATE TABLE IF NOT EXISTS hosdoctor (doctor VARCHAR(255), age VARCHAR(255), ID VARCHAR(255), phone VARCHAR(255))")
+    beginMenu = {'1': register_new_account, '2': login_to_an_existing_account}
+    for key, value in beginMenu.items():
+        print(f"{key} : {value.__name__ }")
+    MenuInput = int(input("Make your choice: "))
+    try:
+        beginMenu[f"{MenuInput}"](occupation)
+    except NameError:
+        beginMenu[f"{MenuInput}"]()
+
